ff2a36508f483597aa1889db3662a5f85c435ad6
Valery <qaivlev@yandex.ru>
2023-12-23 20:27:25 +0300

    firstGen

--- src/test/java/com/zvuk/TestBase.java
+++ src/test/java/com/zvuk/TestBase.java
@@ -27,8 +27,8 @@ public class TestBase {
         Configuration.webdriverLogsEnabled = true;
         Configuration.browser = System.getProperty("browser", "chrome");
 
-        //Configuration.remote
-        //        = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote
+                = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
